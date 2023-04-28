6771736cb2213fab23bd1c2938fd72983028b2b6
iam6adman <iam6adman@outlook.com>
2023-04-28 19:05:37 +0300

    add example 038

---
+++ example038/Program.cs
@@ -0,0 +1,41 @@
+﻿Console.WriteLine("Введите размер массива  ");
+int size = int.Parse(Console.ReadLine());
+double[] num = new double[size];
+ArrayRandomNum(num);
+Console.WriteLine("массив: ");
+PrintArray(num);
+double min = Int32.MaxValue;
+double max = Int32.MinValue;
+
+for (int z = 0; z < num.Length; z++)
+{
+    if (num[z] > max)
+        {
+            max = num[z];
+        }
+    if (num[z] < min)
+        {
+            min = num[z];
+        }
+}
+
+Console.WriteLine($"всего {num.Length} чисел. Максимальное = {max}, минимальное = {min}");
+Console.WriteLine($"Разница между ними = {max - min}");
+
+void ArrayRandomNum(double[] num)
+{
+    for(int i = 0; i < num.Length; i++)
+        {
+            num[i] = Convert.ToDouble(new Random().Next(100,1000)) / 100;
+        }
+}
+void PrintArray(double[] num)
+{
+    Console.Write("[ ");
+    for(int i = 0; i < num.Length; i++)
+        {
+            Console.Write(num[i] + " ");
+        }
+    Console.Write("]");
+    Console.WriteLine();
+}
